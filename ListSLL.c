af5fb5152a9d7e01a807d026a8fa555801a315ba
Sotto <johnperloe.sotto@emerson.com>
2022-12-04 18:39:40 +0800

    Initial Commit.
    This workspace is the first formattive assessment in MCPE631 Data Structures and Algorithm

---
+++ ListSLL.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ListSLL.h"
+
+extern struct node *_firstnode, *_lastnode;
+/*
+_x - value of the node
+_p - position of the specific node in the list to be inserted
+*_L - pointer to the list
+*/
+void insert(int _x,struct node *_L){
+    if(_L == NULL){
+        printf("The node is empty!\n");
+        printf("Creating Node...\n ");
+        create_node(_x);
+        printf("Node creation successful! \n");
+    }
+    else{
+        printf("Creating next node...\n");
+        _lastnode->nxt = (struct node*)malloc(sizeof(struct node));
+        _lastnode->nxt->x = _x;
+        _lastnode->nxt->nxt = NULL;
+        _lastnode = _lastnode->nxt;
+        // _L->nxt = (struct node*)malloc(sizeof(struct node));
+        // nextnode = _L->nxt;
+    }
+    
+    // nextnode = _L;
+    // nextnode->x = _x;
+    // nextnode->nxt = NULL;
+}
+
+void create_node(int _val){
+    _firstnode = (struct node*)malloc(sizeof(struct node));  //allocate a memory location to the node and return the pointer to that memory location to the pointer variable *_node
+    _lastnode = _firstnode;
+    _lastnode->x = _val;
+    _lastnode->nxt = NULL;
+}
---
+++ ListSLL.h
@@ -0,0 +1,12 @@
+#ifndef ListSLL
+#define ListSLL
+
+struct node{
+    int x;
+    struct node *nxt;
+};
+
+void create_node(int _x);
+void insert(int _x,struct node *_L);
+
+#endif
